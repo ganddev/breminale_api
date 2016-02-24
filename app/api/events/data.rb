cdfd9a60d87311561f1ffc914a401116b21721e1
Björn Ahlfeld <bjoern.ahlfeld@trecker.com>
2016-02-24 17:26:21 +0100

    fixed merge conflicts

--- app/api/events/data.rb
+++ app/api/events/data.rb
@@ -8,73 +8,12 @@ module Events
       end
       desc "Returns a single event"
       params do
-<<<<<<< HEAD
         requires :id, type: Integer, desc: "Id of the event"
       end
       get ':id' do
         event = ::Event.find_by_id(id)
         present event, with: Entities::Event
       end
-=======
-        requires :all, using: Entities::Event.documentation
-      end
-      put ':id' do
-        event = Event.find_by_id(params[:id])
-        if event.present?
-            if event.isAllowedToEdit(current_user)
-              Event.update(params[:id],params)
-            else
-              error!("403 - Forbidden to edit event", 403)
-            end  
-        else
-          error!("404 - Event not found", 404) 
-        end 
-      end
-
-      desc "Delete a single event", headers: {
-        "Authorization" => {
-          description: "JWS Token which validates your identity",
-          required: true
-        }
-      }
-      params do 
-        requires :id, type: Integer, desc: "Id of the event"
-      end
-
-      delete ':id' do
-        event = Event.find_by_id(params[:id])
-        if event.present?
-            if can? :delete, event
-              
-            else
-              error!("403 - Forbidden to edit event", 403)
-            end
-        else
-          error!("404 - Event not found", 404)
-        end 
-      end  
-
-
-      desc 'create a new event', headers: {
-        "Authorization" => {
-          description: "JWS Token which validates your identity",
-          required: true
-        }
-      }
-      params do
-        requires :all, except: [:id], using: Entities::Event.documentation.except(:id)
-      end
-      post do
-        if current_user.present?
-      	 @newevent = current_user.events.new({name:params[:name],desc:params[:desc], user:current_user})
-         if newevent.valid?
-           newevent.save
-         end
-        else
-          error!("404 User not found!", 404)
-        end 
-      end	
->>>>>>> 5e69de761d7eafe9c593e816977475afaf55fa98
     end
   end
 end
--- app/api/locations/data.rb
+++ app/api/locations/data.rb
@@ -6,7 +6,6 @@ module Locations
 				locations = Location.all
 				present locations, with: Entities::Location
 			end
-<<<<<<< HEAD
 			desc "Returns a single location"
 			params do 
         		requires :id, type: Integer, desc: "Id of the location"
@@ -15,71 +14,6 @@ module Locations
       			location = ::Location.find_by_id(params[:id])
       			present location, with: Entities::Location
     		end
-=======
-			desc "Creates a location", headers: {
-				"Authorization" => {
-          			description: "JWS Token which validates your identity",
-          			required: true
-        		}
-			}
-			params do
-				requires :all, except: [:id], using: Entities::Location.documentation.except(:id)
-			end
-			post do
-				if current_user.present?
-					{ 'declared_params' => declared(params, include_missing: false) }
-					location = ::Location.create({name: params[:name], latitude: params[:latitude], longitude: params[:longitude], user: current_user, description: params[:description], image: params[:image_url]})
-					byebug
-					if !location.valid?
-						error!('400 error message' + location.errors.messages.to_json, 400)
-					end	
-				end
-			end	
-
-			desc "Updates a location", headers: {
-				"Authorization" => {
-          			description: "JWS Token which validates your identity",
-          			required: true
-        		}
-			}
-			params do
-        		requires :all, using: Entities::Location.documentation
-			end	
-			put ':id' do
-				location = ::Location.find_by_id(parms[:id])
-				if location.present?
-					if location.isAllowedToEditLocation(current_user)
-						updatedLocation = Location.update(params[:id], params)
-					else
-						error!("403 - Not allowed to update location", 403)
-					end	
-				else
-					error!("404 - Location with not found!", 404)
-				end	
-			end
-
-			desc "Deletes a location", headers: {
-				"Authorization" => {
-          			description: "JWS Token which validates your identity",
-          			required: true
-        		}
-			}
-			params do
-				requires :id, type: Integer, desc: "id of the location"
-			end
-			delete ':id' do
-				location = Location.find_by_id(params[:id])
-				if location.present?
-					if location.isAllowedToEditLocation(current_user)
-						location.destroy
-					else
-						error!("403 - Not allowed to delete location")
-					end		
-				else
-					error!("404 - Location with not found", 404)
-				end	
-			end	
->>>>>>> 5e69de761d7eafe9c593e816977475afaf55fa98
 		end
 	end
 end
