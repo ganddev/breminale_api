d4bb3c50470514dffdb8c48bfbbd1806d7dba07c
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2016-03-17 21:42:44 +0100

    Links for location set --skip-ci

--- app/controllers/profile/locations_controller.rb
+++ app/controllers/profile/locations_controller.rb
@@ -27,7 +27,7 @@ class Profile::LocationsController < ApplicationController
     @location = current_user.locations.create!(location_params)
     respond_to do |format|
       if @location.save
-        format.html { redirect_to @profile_location, notice: 'Location was successfully created.' }
+        format.html { redirect_to [:profile, @location], notice: 'Location was successfully created.' }
         format.json { render :show, status: :created, location: @location }
       else
         format.html { render :new }
@@ -41,7 +41,7 @@ class Profile::LocationsController < ApplicationController
   def update
     respond_to do |format|
       if @location.update(location_params)
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.html { redirect_to [:profile, @location], notice: 'Location was successfully updated.' }
         format.json { render :show, status: :ok, location: @location }
       else
         format.html { render :edit }
