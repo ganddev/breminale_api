38422358979b4c68bf9d17695028865b6ed01209
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2016-03-19 13:49:32 +0100

    Added time field for event --skip-ci

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ActiveRecord::Base
 	belongs_to :location
 	belongs_to :date
 	validates :name, presence: true
+	validates :start_time, presence: true
 	validates :description, presence: true
 	validates :location, presence: true
 	validates :date, presence: true
