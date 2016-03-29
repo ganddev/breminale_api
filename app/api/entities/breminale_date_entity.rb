b2d4e6fbefde18b1141fe950349a5ea8770ce414
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2016-03-29 22:01:14 +0200

    Changed date entity

--- app/api/entities/breminale_date_entity.rb
+++ app/api/entities/breminale_date_entity.rb
@@ -1,6 +1,6 @@
 module Entities
 	class Entities::BreminaleDateEntity < Grape::Entity
 		expose :id, documentation: {type: "Integer", desc: "Id of the date"}
-		expose :date_day.strftime("%d.%m.%Y") , documentation: {type: "String", desc: "Date as ISO String"}
+		expose :date_day, documentation: {type: "String", desc: "Date as ISO String"}
 	end
 end
