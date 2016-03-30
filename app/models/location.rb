2280696755d9e441c31bccfd24fd806d672628f6
Björn Ahlfeld <bjoern.ahlfeld@trecker.com>
2016-03-30 16:19:03 +0200

    Added aws s3 stuff for image upload --skip-ci

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,11 @@
 class Location < ActiveRecord::Base
+	
+	has_attached_file :image_url, styles: {
+    	thumb: '100x100>',
+    	square: '200x200#',
+    	medium: '300x300>'
+  	}
+
 	belongs_to :user
 	has_many :events, dependent: :destroy
 	validates :name, presence: true
