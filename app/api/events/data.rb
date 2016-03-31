07233b98bcfe882f7f59ed7e131de549f22921f9
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2016-03-31 19:44:46 +0200

    Added response model to documentation

--- app/api/events/data.rb
+++ app/api/events/data.rb
@@ -1,14 +1,18 @@
 module Events
   class Data < Grape::API
     resource :events do
-      desc "List all events"
+      desc "List all events" do
+        success Entities::EventEntity
+      end
       get do
         events  = ::Event.all
         present events, with: Entities::EventEntity
       end
-      desc "Returns a single event"
+      desc "Returns a single event" do
+        success Entities::EventEntity
+      end
       params do
-        requires :id, type: Integer, desc: "Id of the event"
+          requires :id, type: Integer, desc: "Id of the event"
       end
       get ':id' do
         event = ::Event.find_by_id!(params[:id])
--- app/api/locations/data.rb
+++ app/api/locations/data.rb
@@ -6,14 +6,16 @@ module Locations
 				locations = ::Location.all
 				present locations, with: Entities::LocationEntity
 			end
-			desc "Returns a single location"
+			desc "Returns a single location" do
+				success Entities::LocationEntity
+			end
 			params do 
-        		requires :id, type: Integer, desc: "Id of the location"
-      		end
-      		get ':id' do
-      			location = ::Location.find_by_id!(params[:id])
-      			present location, with: Entities::LocationEntity
-    		end
+        requires :id, type: Integer, desc: "Id of the location"
+      end
+      get ':id' do
+      	location = ::Location.find_by_id!(params[:id])
+      	present location, with: Entities::LocationEntity
+    	end
 		end
 	end
 end
