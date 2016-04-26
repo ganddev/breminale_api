868777c299580f434847f9b921dff124deb5c4c8
Björn Ahlfeld <bjoern.ahlfeld@trecker.com>
2016-04-26 16:56:18 +0200

    Added migration for apns

---
+++ db/migrate/20160426132757_register_app_apns.rb
@@ -0,0 +1,11 @@
+class RegisterAppApns < ActiveRecord::Migration
+  def change
+  	app = Rpush::Apns::App.new
+	app.name = "ios_app"
+	app.certificate = File.read("/path/to/sandbox.pem")
+	app.environment = "sandbox" # APNs environment.
+	app.password = Rails.application.secrets.apns_password
+	app.connections = 1
+	app.save!
+  end
+end
